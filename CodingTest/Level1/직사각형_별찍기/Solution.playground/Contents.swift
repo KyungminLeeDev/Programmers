9207e37894342bd2d8bf3e63505add0ab6c305b6
KyungminLee <kyungmin.lee.dev@gmail.com>
2020-06-14 18:38:31 +0900

    솔루션 파일 추가

---
+++ CodingTest/Level1/직사각형_별찍기/Solution.playground/Contents.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
+let (a, b) = (n[0], n[1])
+var star: String = ""
+
+var stars: String = ""
+
+for _ in 1...b {
+    for _ in 1...a {
+        stars += "*"
+    }
+    stars += "\n"
+}
+print(stars)
