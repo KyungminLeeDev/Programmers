b18e0ef8bc9a62cca8eea044a07ff1a7b423e2c7
KyungminLee <kyungmin.lee.dev@gmail.com>
2020-06-22 19:39:38 +0900

    제출한 코드 수정

--- CodingTest/Level1/09_정수내림차순으로배치하기/Solution.playground/Contents.swift
+++ CodingTest/Level1/09_정수내림차순으로배치하기/Solution.playground/Contents.swift
@@ -1,19 +1,17 @@
 func solution(_ n:Int64) -> Int64 {
     var arr = [Int64]()
-    var div: Int64 = 0
-    var num: Int64 = 0
+    var num = n
     
-    div = 1
-    while div < n {
-        arr.append((n % (div * 10)) / div)
-        div *= 10
+    while num > 0 {
+        arr.append(num % 10)
+        num /= 10
     }
     arr.sort()
     
-    
-    div = 1
+    num = 0
+    var div = 1
     for i in arr {
-        num += (i * div)
+        num += (i * Int64(div))
         div *= 10
     }
     
