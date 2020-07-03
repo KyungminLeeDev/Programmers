5b74f5c6da37e0a0943d05d1c5ba61bceed41a6a
KyungminLee <kyungmin.lee.dev@gmail.com>
2020-07-03 20:12:48 +0900

    Solution 추가

--- CodingTest/Level2/06_피보나치수/Solution.playground/Contents.swift
+++ CodingTest/Level2/06_피보나치수/Solution.playground/Contents.swift
@@ -1,3 +1,15 @@
-import UIKit
+func solution(_ n:Int) -> Int {
+    var fibo = [0, 1]
+    var result = 0
 
-var str = "Hello, playground"
+    for _ in 1..<n {
+        result = (fibo[0] + fibo[1]) % 1234567
+        fibo[0] = fibo[1]
+        fibo[1] = result
+    }
+
+    return result
+}
+
+print(solution(3))
+print(solution(5))
