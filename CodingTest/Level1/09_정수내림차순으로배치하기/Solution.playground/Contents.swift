9ebfa7087cb51a5069e04f6460e3c47d2ea07c22
KyungminLee <kyungmin.lee.dev@gmail.com>
2020-07-10 14:40:54 +0900

    다시 푼 코드 추가

--- CodingTest/Level1/09_정수내림차순으로배치하기/Solution.playground/Contents.swift
+++ CodingTest/Level1/09_정수내림차순으로배치하기/Solution.playground/Contents.swift
@@ -1,22 +1,28 @@
+//func solution(_ n:Int64) -> Int64 {
+//    var arr = [Int64]()
+//    var num = n
+//
+//    while num > 0 {
+//        arr.append(num % 10)
+//        num /= 10
+//    }
+//    arr.sort()
+//
+//    num = 0
+//    var div = 1
+//    for i in arr {
+//        num += (i * Int64(div))
+//        div *= 10
+//    }
+//
+//    return num
+//}
+
+// 다시 푼 코드
 func solution(_ n:Int64) -> Int64 {
-    var arr = [Int64]()
-    var num = n
-    
-    while num > 0 {
-        arr.append(num % 10)
-        num /= 10
-    }
-    arr.sort()
-    
-    num = 0
-    var div = 1
-    for i in arr {
-        num += (i * Int64(div))
-        div *= 10
-    }
-    
-    return num
+    return Int64(String(Array(String(n)).sorted(by: >)))!
 }
+
 
 print(solution(118372))
 
